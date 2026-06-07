5b57f030338bce163e852d355585a61a64fa98bb
Sebastian Diaz <sqdiaz@icloud.com>
2026-06-08 00:39:38 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Philippine Universities Freedom Wall</h1>
+      <h1 className="text-4xl font-bold">MyFreedomWall</h1>
       <p className="mt-4 text-xl text-center">
-        A safe space for students to share their thoughts and experiences.
+        THE space for students/alumni to discuss and share about anything and everything university.
       </p>
     </main>
   )
